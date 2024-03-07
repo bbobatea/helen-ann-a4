ab2c85503236f2a4bca6f323d0c01e285046711b
bbobatea <133336237+bbobatea@users.noreply.github.com>
2024-03-07 00:42:24 -0800

    Add files via upload

---
+++ log-sleep/log-sleep.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OvernightSleepData } from '../data/overnight-sleep-data';
+
+@Component({
+  selector: 'app-log-sleep',
+  templateUrl: './log-sleep.component.html',
+  styleUrls: ['./log-sleep.component.scss'],
+})
+export class LogSleepComponent implements OnInit {
+  startTime: Date | null = null;
+  endTime: Date | null = null;
+  overnightSleepData: OvernightSleepData | null = null;
+  diff: string | null = null;
+  isStartTimeSet: boolean = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {}
+
+  updateStartTime() {
+    this.startTime = new Date();
+    this.isStartTimeSet = true;
+    console.log("start: ", this.startTime);
+  }
+
+  updateEndTime() {
+    this.endTime = new Date();
+    this.isStartTimeSet = false;
+    console.log("false: ", this.endTime);
+  }
+
+  OvernightSleep() {
+    if (this.startTime && this.endTime) {
+      // Create an instance of OvernightSleepData using start and end times
+      this.overnightSleepData = new OvernightSleepData(this.startTime, this.endTime);
+      console.log("id: ", this.overnightSleepData.id);
+      console.log('Overnight Sleep Data:', this.overnightSleepData);
+    } else {
+      console.log('Please specify both start and end times.');
+    }
+  }
+
+  totalHours(): string {
+    if (this.startTime && this.endTime) {
+      var sleepStart_ms = this.startTime.getTime();
+      var sleepEnd_ms = this.endTime.getTime();
+      var difference_ms = sleepEnd_ms - sleepStart_ms;
+      this.diff = Math.floor(difference_ms / (1000*60*60)) + " hours, " + Math.floor(difference_ms / (1000*60) % 60) + " minutes.";
+      console.log("summary: ", this.diff);
+      return this.diff;
+    } else {
+      return '';
+    }
+  }
+
+	goToHome() {
+		this.router.navigate(['/home']);
+	}
+
+	logSleep() {
+	this.router.navigate(['/log-sleep-component']);
+	const currentDateTime = new Date();
+	}
+
+	viewData() {
+		this.router.navigate(['/data-view-component']);
+	}
+
+	goToSettings() {
+		this.router.navigate(['/settings-component']);
+	}
+
+	goToGoals() {
+		this.router.navigate(['/sleep-goals-component']);
+	}
+
+}
