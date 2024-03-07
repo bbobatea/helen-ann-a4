91b1d4406b549c2299bf78b55601f3f72d11107b
bbobatea <133336237+bbobatea@users.noreply.github.com>
2024-03-07 00:41:23 -0800

    Add files via upload

---
+++ data-view/data-view.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-data-view',
+  templateUrl: './data-view.component.html',
+  styleUrls: ['./data-view.component.scss'],
+})
+export class DataViewComponent  implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {}
+
+  goToHome() {
+		this.router.navigate(['/home']);
+	}
+
+	logSleep() {
+	this.router.navigate(['/log-sleep-component']);
+	const currentDateTime = new Date();
+	}
+
+	viewData() {
+		this.router.navigate(['/data-view-component']);
+	}
+
+	goToSettings() {
+		this.router.navigate(['/settings-component']);
+	}
+
+	goToGoals() {
+		this.router.navigate(['/sleep-goals-component']);
+	}
+
+}
