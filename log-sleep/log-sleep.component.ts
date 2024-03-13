03e3aacdaabc0c112068ac4f226ed67063e7db9b
bbobatea <133336237+bbobatea@users.noreply.github.com>
2024-03-13 00:58:44 -0700

    Update log-sleep.component.ts

--- log-sleep/log-sleep.component.ts
+++ log-sleep/log-sleep.component.ts
@@ -146,20 +146,20 @@ export class LogSleepComponent implements OnInit {
 	goToHome() {
 		this.router.navigate(['/home']);
 	}
-	
+
 	logSleep() {
 	this.router.navigate(['/log-sleep-component']);
 	const currentDateTime = new Date();
 	}
-	
+
 	viewData() {
 		this.router.navigate(['/data-view-component']);
 	}
-	
+
 	goToSettings() {
 		this.router.navigate(['/settings-component']);
 	}
-	
+
 	goToGoals() {
 		this.router.navigate(['/sleep-goals-component']);
 	}
