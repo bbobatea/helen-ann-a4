8ec13d255d2d12bb391346546e1e9d4d31b0c692
bbobatea <133336237+bbobatea@users.noreply.github.com>
2024-03-13 00:59:40 -0700

    Update display-logged-moods.page.ts

--- display-logged-moods/display-logged-moods.page.ts
+++ display-logged-moods/display-logged-moods.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Preferences } from '@capacitor/preferences';
+import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
 
 @Component({
   selector: 'app-display-logged-moods',
@@ -9,6 +11,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class DisplayLoggedMoodsPage implements OnInit {
   data: any;
   stanfordSleepinessList: any;
+  username: string = '';
+	sleepyData: StanfordSleepinessData[] = [];
 
   constructor(private router: Router, private route: ActivatedRoute) {
     const navigation = this.router.getCurrentNavigation();
@@ -18,10 +22,34 @@ export class DisplayLoggedMoodsPage implements OnInit {
     }
   }
 
-  ngOnInit() {
-    console.log(this.stanfordSleepinessList);
+  async ngOnInit() {
+    await this.retrieveUsername();
+      this.showLoggedMoods();
+    }
+
+
+  async retrieveUsername() {
+    try {
+      const { value } = await Preferences.get({ key: 'username' });
+      if (value) {
+        this.username = value;
+        console.log("this.username: ", this.username);
+      } else {
+        console.log('No username found in storage');
+      }
+    } catch (error) {
+      console.error('Error retrieving username:', error);
+    }
   }
 
+  async showLoggedMoods() {
+    const { value : moodValue } = await Preferences.get({ key: this.username + "_loggedMood" });
+    if (moodValue) {
+      this.sleepyData = JSON.parse(moodValue).map((item: any) => {
+        return new StanfordSleepinessData(item.loggedValue, new Date(item.loggedAt));
+      });
+	}
+  }
   formatDate(dateTime: Date): string {
     const options: Intl.DateTimeFormatOptions = {
       year: 'numeric',
@@ -33,4 +61,25 @@ export class DisplayLoggedMoodsPage implements OnInit {
     };
     return new Date(dateTime).toLocaleString('en-US', options);
   }
+	goToHome() {
+		this.router.navigate(['/home']);
+	}
+
+	logSleep() {
+	this.router.navigate(['/log-sleep-component']);
+	const currentDateTime = new Date();
+	}
+
+	viewData() {
+		this.router.navigate(['/data-view-component']);
+	}
+
+	goToSettings() {
+		this.router.navigate(['/settings-component']);
+	}
+
+	goToGoals() {
+		this.router.navigate(['/sleep-goals-component']);
+	}
+
 }
