b27c312b8ac83a98ef3e6862257c2f111548efae
bbobatea <133336237+bbobatea@users.noreply.github.com>
2024-03-14 03:51:19 -0700

    Update settings.component.ts

--- settings/settings.component.ts
+++ settings/settings.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Preferences } from '@capacitor/preferences';
+import { ModalController } from '@ionic/angular';
+import { UpdatePasswordComponent } from '../update-password/update-password.component';
+import { DeleteAccountPage } from '../delete-account/delete-account.page';
 
 @Component({
   selector: 'app-settings',
@@ -9,10 +12,8 @@ import { Preferences } from '@capacitor/preferences';
 })
 export class SettingsComponent  implements OnInit {
 	username: string = '';
-	newPassword: string = '';
 
-  constructor(private router: Router) { }
-
+  constructor(private router: Router, private modalcontroller: ModalController) { }
   async ngOnInit() {
 	await this.retrieveUsername();
   }
@@ -35,13 +36,24 @@ export class SettingsComponent  implements OnInit {
     this.router.navigate(['/login']);
   }
 
-  async changePassword() {
-	try {
-        await Preferences.set({ key: this.username, value: this.newPassword });
-        console.log('Password updated successfully.');
-    } catch (error) {
-        console.error('Error updating username:', error);
-    }
+  async openChangePasswordModal() {
+    const modal = await this.modalcontroller.create({
+      component: UpdatePasswordComponent,
+      componentProps: {
+        username: this.username, 
+      },
+    });
+    return await modal.present();
+  }
+
+  async delete() {
+    const modal = await this.modalcontroller.create({
+      component: DeleteAccountPage,
+      componentProps: {
+        username: this.username, 
+      },
+    });
+    return await modal.present();
   }
 
 	goToHome() {
