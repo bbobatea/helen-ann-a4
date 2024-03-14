62e1c06ef904abdd1d52d1b97fab47cbb6c1d0c5
4nnn3x <156854487+4nnn3x@users.noreply.github.com>
2024-03-14 00:16:48 -0700

    Update home.page.ts

--- home/home.page.ts
+++ home/home.page.ts
@@ -5,6 +5,7 @@ import { OvernightSleepData } from '../data/overnight-sleep-data';
 import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
 import { Router } from '@angular/router';
 import { LocalNotifications } from '@capacitor/local-notifications';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-home',
@@ -12,13 +13,61 @@ import { LocalNotifications } from '@capacitor/local-notifications';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-	
-  constructor(public sleepService:SleepService, private router: Router) {
+	stanfordSleepiness: StanfordSleepinessData | null = null;
+	loggedMood: number = 1;
+	username: string = '';
+
+  constructor(private router: Router) {
 	}
 
 	ngOnInit() {
-		console.log(this.allSleepData);
+		this.retrieveUsername();
 	}
+
+	async retrieveUsername() {
+		try {
+		  const { value } = await Preferences.get({ key: 'username' });
+		  if (value) {
+			this.username = value;
+			console.log("this.username: ", this.username);
+		  } else {
+			console.log('No username found in storage');
+		  }
+		} catch (error) {
+		  console.error('Error retrieving username:', error);
+		}
+	  }
+
+
+	async saveLoggedMood() {
+		this.stanfordSleepiness = new StanfordSleepinessData(this.loggedMood);
+		try {
+		  const existingData = await Preferences.get({ key: this.username + "_loggedMood"});
+		  console.log("existing data: ", existingData);
+		  let newMoodData: StanfordSleepinessData[] = [];
+		  if (existingData.value) {
+			newMoodData = JSON.parse(existingData.value).map((item: any) => {
+			  return new StanfordSleepinessData(item.loggedValue, new Date(item.loggedAt));
+			});
+		  }
+		  newMoodData.push(this.stanfordSleepiness);
+		  console.log("updated mood data: ", newMoodData);
+		  await Preferences.set({ key: this.username + '_loggedMood', value: JSON.stringify(newMoodData) });
+		  console.log('Logged mood saved');
+		  
+		  // Log retrieved mood data after saving
+		  const { value } = await Preferences.get({ key: this.username + "_loggedMood" });
+		  if (value) {
+			const retrievedMood = JSON.parse(value);
+			console.log('Retrieved mood:', retrievedMood);
+		  } else {
+			console.log('No mood data found in storage');
+		  }
+		} catch (error) {
+		  console.error('Error saving logged mood: ', error);
+		}
+	}
+
 	async scheduleNotification() {
 		await LocalNotifications.schedule({
 		  notifications: [
@@ -31,6 +80,8 @@ export class HomePage {
 		  ]
 		});
 	}
+
+	
 	/* Ionic doesn't allow bindings to static variables, so this getter can be used instead. */
 	get allSleepData() {
 		return SleepService.AllSleepData;
