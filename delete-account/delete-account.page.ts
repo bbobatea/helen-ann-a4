38f9e1d4e99885e83df0edcf01e8f3f0d48cf1ee
bbobatea <133336237+bbobatea@users.noreply.github.com>
2024-03-14 03:50:16 -0700

    Add files via upload

---
+++ delete-account/delete-account.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-delete-account',
+  templateUrl: './delete-account.page.html',
+  styleUrls: ['./delete-account.page.scss'],
+})
+export class DeleteAccountPage implements OnInit {
+  constructor(private modalController: ModalController, private router: Router) {}
+  username: string = '';
+	newPassword: string = '';
+
+  ngOnInit() {
+    this.retrieveUsername();
+  }
+
+  async retrieveUsername() {
+    try {
+      const { value } = await Preferences.get({ key: 'activeUser' });
+      if (value) {
+        this.username = value;
+        console.log("this.username: ", this.username);
+      } else {
+        console.log('No username found in storage');
+      }
+    } catch (error) {
+      console.error('Error retrieving username:', error);
+    }
+  }
+
+  async deleteValue() {
+    try {
+      await Preferences.remove({ key: this.username });
+      await Preferences.remove({ key: this.username + " data"});
+      await Preferences.remove({ key: this.username + "_loggedMood"});
+      console.log('Value deleted successfully');
+      this.router.navigate(['/login']);
+      this.cancel();
+
+    } catch (error) {
+      console.error('Error deleting value:', error);
+    }
+  }
+
+  async cancel() {
+    await this.modalController.dismiss();
+  }
+
+
+}
