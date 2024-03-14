806f07f2362a9a6ba328d805b2075fabed23bc07
bbobatea <133336237+bbobatea@users.noreply.github.com>
2024-03-14 00:50:38 -0700

    Update settings.component.ts

--- settings/settings.component.ts
+++ settings/settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-settings',
@@ -7,10 +8,41 @@ import { Router } from '@angular/router';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent  implements OnInit {
+	username: string = '';
+	newPassword: string = '';
 
   constructor(private router: Router) { }
 
-  ngOnInit() {}
+  async ngOnInit() {
+	await this.retrieveUsername();
+  }
+
+  async retrieveUsername() {
+    try {
+      const { value } = await Preferences.get({ key: 'activeUser' });
+      if (value) {
+        this.username = value;
+        console.log("this.username: ", this.username);
+      } else {
+        console.log('No username found in storage');
+      }
+    } catch (error) {
+      console.error('Error retrieving username:', error);
+    }
+  }
+
+  signOut() {
+    this.router.navigate(['/login']);
+  }
+
+  async changePassword() {
+	try {
+        await Preferences.set({ key: this.username, value: this.newPassword });
+        console.log('Password updated successfully.');
+    } catch (error) {
+        console.error('Error updating username:', error);
+    }
+  }
 
 	goToHome() {
 		this.router.navigate(['/home']);
